2534d053a10eb2115f8f9c398a63356e487bd24d
nanntew <wsetxbzd0@gmail.com>
2026-06-18 11:01:38 +0330

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const glow = document.querySelector('.cursor-glow');
-
-document.addEventListener('mousemove',(e)=>{
-
-glow.style.left = e.clientX - 150 + 'px';
-glow.style.top = e.clientY - 150 + 'px';
-
-});
